864c8722c92a8306786777922880ea70dcebe916
Suta Lite <nedje28@gmail.com>
2021-06-16 16:05:26 +0200

    Ajout de ModeleTableHauteurs

---
+++ src/modele/ModeleTableHauteurs.java
@@ -0,0 +1,26 @@
+package modele;
+
+import javax.swing.table.DefaultTableModel;
+
+public class ModeleTableHauteurs extends DefaultTableModel {
+
+	public ModeleTableHauteurs(Maree[] marees) {
+		super();
+		setColumnCount(6);
+		setRowCount(8);
+
+		int row = 0;
+		int column = 0;
+		for (Maree m : marees) {
+			setValueAt(m.getHeure(), row, column);
+			setValueAt(m.getHauteur() + "m", row + 1, column);
+			// On avance la colonne après chaque ajout
+			column++;
+			// Et on ajoute 2 ligne afin de sauter l'heure et la date
+			if (column % 6 == 0) {
+				row += 2;
+				column = 0;
+			}
+		}
+	}	
+}
--- src/vue/PanelHauteurs.java
+++ src/vue/PanelHauteurs.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 
 import javax.swing.JPanel;
 import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
 
 import modele.Maree;
 import modele.MareeHauteur;
+import modele.ModeleTableHauteurs;
 
 public class PanelHauteurs extends JPanel {
 
@@ -15,24 +15,8 @@ public class PanelHauteurs extends JPanel {
 	JTable table;
 	public PanelHauteurs(ArrayList<MareeHauteur> _marees) {
 		this.marees = _marees;
-		DefaultTableModel model = new DefaultTableModel();
-		model.setColumnCount(6);
-		model.setRowCount(8);
-
-		int row = 0;
-		int column = 0;
-		Maree[] ms = marees.get(0).getMarees();
-		for (int i = 0; i < ms.length; i++) {
-			Maree m = ms[i];
-			model.setValueAt(m.getHeure(), row, column);
-			model.setValueAt(m.getHauteur() + "m", row + 1, column);
-			column++;
-			if (column % 6 == 0) {
-				row += 2;
-				column = 0;
-			}
-		}
-		
+		Maree[] mareesHauteurs = marees.get(0).getMarees();
+		ModeleTableHauteurs model = new ModeleTableHauteurs(mareesHauteurs);
 		JTable table = new JTable(model);
 		add(table);
 	}
