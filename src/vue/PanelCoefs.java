002072bbabf7880d8195e0f2d52b5e0ebe9977df
Suta Lite <nedje28@gmail.com>
2021-06-19 14:26:22 +0200

    Extraction du tableau des marees

--- src/vue/PanelCoefs.java
+++ src/vue/PanelCoefs.java
@@ -1,23 +1,9 @@
 package vue;
 
-import javax.swing.JPanel;
-import javax.swing.JTable;
-
-import constantes.ConstantesCouleurs;
 import modele.Maree;
-import modele.MareeCellRenderer;
 import modele.ModeleTableCoef;
 
-public class PanelCoefs extends JPanel {
-	JTable table;
-	
-	public PanelCoefs() {
-		table = new JTable();
-		table.setDefaultRenderer(Object.class, new MareeCellRenderer());
-		add(table);
-		setBackground(ConstantesCouleurs.MASTIC);
-	}
-	
+public class PanelCoefs extends PanelMaree {
 	public void setValeurs(Maree[] marees) {
 		ModeleTableCoef model = new ModeleTableCoef(marees);
 		table.setModel(model);
--- src/vue/PanelHauteurs.java
+++ src/vue/PanelHauteurs.java
@@ -1,23 +1,9 @@
 package vue;
 
-import javax.swing.JPanel;
-import javax.swing.JTable;
-
-import constantes.ConstantesCouleurs;
 import modele.Maree;
-import modele.MareeCellRenderer;
 import modele.ModeleTableHauteurs;
 
-public class PanelHauteurs extends JPanel {
-	JTable table;
-	
-	public PanelHauteurs() {
-		table = new JTable();
-		table.setDefaultRenderer(Object.class, new MareeCellRenderer());  
-		add(table);
-		setBackground(ConstantesCouleurs.ROUGE);
-	}
-	
+public class PanelHauteurs extends PanelMaree {
 	public void setValeurs(Maree[] marees) {
 		ModeleTableHauteurs model = new ModeleTableHauteurs(marees);
 		table.setModel(model);
